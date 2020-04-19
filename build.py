d8ec3900cc71e143ff813e774f6bb6d1dc5b5604
Rob Stenson <rob@goodhertz.com>
2020-04-19 11:40:22 -0700

    initial

---
+++ build.py
@@ -0,0 +1,32 @@
+import markdown, jinja2
+from pathlib import Path
+from shutil import copytree
+from time import time
+from datetime import datetime
+
+postsdir = Path("posts")
+posts = []
+
+for post in reversed(sorted(postsdir.glob("*.md"))):
+    html = markdown.markdown(post.read_text(), extensions=["smarty"])
+    posts.append([post.stem, html])
+
+print([p[0] for p in posts])
+
+build_time = datetime.utcfromtimestamp(int(time())).strftime("%a, %d %b %Y %H:%M:%S %z")
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+index_template = env.get_template("index.j2")
+feed_template = env.get_template("feed.j2")
+post_template = env.get_template("post.j2")
+
+sitedir = Path("_site")
+sitedir.mkdir(exist_ok=True)
+
+copytree(Path("assets"), sitedir / "assets", dirs_exist_ok=True)
+(sitedir / "index.html").write_text(index_template.render(dict(posts=posts)))
+(sitedir / "feed.xml").write_text(feed_template.render(dict(build=build_time, posts=posts)))
+(sitedir / "posts").mkdir(exist_ok=True)
+
+for post in posts:
+    (sitedir / f"posts/{post[0]}.html").write_text(post_template.render(dict(post=post)))
