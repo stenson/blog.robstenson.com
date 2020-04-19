5bcb509fafc0c30a693e82c544e7634fdebed7f8
Rob Stenson <rob@goodhertz.com>
2020-04-19 16:11:27 -0700

    len lye tab

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
-aws s3 --region us-west-2 sync --cache-control no-cache --exclude "*" --include "*.html" . s3://blog.robstenson.com --profile personal
-aws s3 --region us-west-2 sync . s3://blog.robstenson.com --profile personal
+aws s3 --region us-west-2 sync --cache-control no-cache --exclude "*" --include "*.html" _site s3://blog.robstenson.com --profile personal
+aws s3 --region us-west-2 sync _site s3://blog.robstenson.com --profile personal
